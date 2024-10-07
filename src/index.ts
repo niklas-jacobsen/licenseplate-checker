59777ef9027f35e1cbf36113ed5b7951ce1905e6
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2024-10-07 22:16:43 +0200

    add request route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
 
 const app = new Hono();
+const prisma = new PrismaClient();
+
+const testRequest = {
+  city: "MS",
+  letters: "N?",
+  numbers: "1?",
+};
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text("App is running");
+});
+
+app.post("/request", async (c) => {
+  await prisma.numberplateRequest.create({
+    data: {
+      city: testRequest.city,
+      letters: testRequest.letters,
+      numbers: testRequest.numbers,
+    },
+  });
+  return c.text("User added");
 });
 
 export default app;
