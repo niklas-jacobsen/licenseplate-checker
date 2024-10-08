d9cb29eb7dbe213ad5008c87d1fb8b9d3e9e39c3
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2024-10-08 21:52:47 +0200

    add zod input validation

--- src/routes/plateRequest.routes.ts
+++ src/routes/plateRequest.routes.ts
@@ -1,39 +1,46 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
 import numberplateRequestController from "../controllers/numberPlateRequestController";
 
 export const numberPlateRequestRouter = new Hono();
 
 const requestController = new numberplateRequestController();
-const testRequest = {
-  city: "MS",
-  letters: "N?",
-  numbers: "1?",
-};
 
-numberPlateRequestRouter.post("/new", async (c) => {
-  const body = await c.req.json();
-
-  if (
-    await requestController.getById({
-      city: body.id.city,
-      letterRequest: body.id.letterRequest,
-      numberRequest: body.id.numberRequest,
-    })
-  )
-    return c.json(
-      { message: "A request with these parameters already exists" },
-      400
-    );
-
-  await requestController.create({
-    city: body.id.city,
-    letterRequest: body.id.letterRequest,
-    numberRequest: body.id.numberRequest,
-  });
-  return c.json(
-    {
-      message: `Request ${body.id.city}-${body.id.letterRequest}-${body.id.numberRequest} was created successfully`,
-    },
-    200
-  );
+const zRequestScheme = z.object({
+  city: z.string(),
+  letters: z.string(),
+  numbers: z.string(),
 });
+
+numberPlateRequestRouter.post(
+  "/new",
+  zValidator("json", zRequestScheme),
+  async (c) => {
+    const body = await c.req.valid("json");
+
+    if (
+      await requestController.getById({
+        city: body.city,
+        letterRequest: body.letters,
+        numberRequest: body.numbers,
+      })
+    )
+      return c.json(
+        { message: "A request with these parameters already exists" },
+        400
+      );
+
+    await requestController.create({
+      city: body.city,
+      letterRequest: body.letters,
+      numberRequest: body.numbers,
+    });
+    return c.json(
+      {
+        message: `Request ${body.city}-${body.letters}-${body.numbers} was created successfully`,
+      },
+      200
+    );
+  }
+);
