77ea2ddc2d585eb952ee1a50ab147f99e3a1a144
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2024-10-30 22:12:38 +0100

    add secure headers hono package

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
+import { secureHeaders } from "hono/secure-headers";
 import router from "./routes";
 
 const app = new Hono();
+app.use(secureHeaders());
 
 app.route("", router);
 export default app;
