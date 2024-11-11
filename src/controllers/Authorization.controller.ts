58aa9643e14b3aa8ae38914b0d7fd322c924e427
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2024-11-11 20:16:40 +0100

    remove duplicate zSchema

--- src/controllers/Authorization.controller.ts
+++ src/controllers/Authorization.controller.ts
@@ -1,14 +1,6 @@
 import { Secret, verify, sign } from 'jsonwebtoken';
 import * as bcrypt from 'bcrypt';
-import { z } from 'zod';
 import { SECRET_KEY } from '../middleware/auth';
-
-export const zodSchema = z.object({
-  email: z.string().email({ message: 'Invalid email address' }),
-  password: z
-    .string()
-    .min(8, { message: 'Password must be at least 8 characters long' }),
-});
 
 class AuthController {
   async hashPassword(plainPassword: string, saltRounds: number = 10) {
