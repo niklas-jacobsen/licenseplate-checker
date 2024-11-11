c8431ea0326b503af7e1f4b0ddc75b53b05670e3
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2024-11-11 20:00:14 +0100

    add rate limiter

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-import { Hono } from "hono";
-import { secureHeaders } from "hono/secure-headers";
-import router from "./routes";
+import { Hono } from 'hono';
+import { secureHeaders } from 'hono/secure-headers';
+import limiter from './middleware/rateLimiter';
+import router from './routes';
 
 const app = new Hono();
 app.use(secureHeaders());
+app.use(limiter);
 
-app.route("", router);
+app.route('', router);
 export default app;
---
+++ src/middleware/rateLimiter.ts
@@ -0,0 +1,10 @@
+import { rateLimiter } from 'hono-rate-limiter';
+
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 50, // Limit each IP to 100 requests per `window`
+  standardHeaders: 'draft-6',
+  keyGenerator: (c) => c.req.header('x-forwarded-for') ?? '',
+});
+
+export default limiter;
