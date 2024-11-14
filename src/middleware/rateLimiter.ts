05f20b4e0f47b48c135afa5d4a21578eb48dfba3
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2024-11-14 14:01:32 +0100

    update rate limit

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -2,7 +2,7 @@ import { rateLimiter } from 'hono-rate-limiter';
 
 const limiter = rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 50, // Limit each IP to 100 requests per `window`
+  limit: 100, // Limit each IP to 100 requests per `window`
   standardHeaders: 'draft-6',
   keyGenerator: (c) => c.req.header('x-forwarded-for') ?? '',
 });
