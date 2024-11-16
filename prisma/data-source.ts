96b905cd3c37765e928c9350cb8727001f5a25fd
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2024-11-16 13:49:29 +0100

    update data source and remove now unneeded dotenv package

--- prisma/data-source.ts
+++ prisma/data-source.ts
@@ -1,17 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 import * as dotenv from 'dotenv';
 import { PrismockClient } from 'prismock';
-
-dotenv.config();
+import { ENV } from '../src/env';
 
 let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'test') {
+if (ENV.NODE_ENV === 'test') {
   console.log('Using mock client');
   prisma = new PrismockClient();
 } else {
   prisma = new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
+    datasourceUrl: ENV.DATABASE_URL,
   });
 }
 
