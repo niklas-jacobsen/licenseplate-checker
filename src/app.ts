8fa30364630ec0103f1267fa4d59f3f51ff497d7
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2024-11-16 03:30:22 +0100

    explicitly state secure headers

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { Hono, Context, Next } from 'hono';
+import { Hono } from 'hono';
 import { secureHeaders } from 'hono/secure-headers';
 import auth from './middleware/auth';
 import limiter from './middleware/rateLimiter';
@@ -6,7 +6,15 @@ import corsMiddleware from './middleware/cors';
 import router from './routes';
 
 const app = new Hono();
-app.use(secureHeaders());
+app.use(
+  secureHeaders({
+    xContentTypeOptions: 'nosniff',
+    crossOriginOpenerPolicy: 'same-origin',
+    referrerPolicy: 'no-referrer',
+    xXssProtection: false,
+    xFrameOptions: false,
+  })
+);
 app.use(limiter);
 app.use(corsMiddleware);
 app.use('/user/*', auth);
