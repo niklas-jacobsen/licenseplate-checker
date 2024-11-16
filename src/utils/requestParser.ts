b71d1890011b617cae0bacf6082f351eb4163244
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2024-11-16 03:29:27 +0100

    clarify remaining stale code

--- src/utils/requestParser.ts
+++ src/utils/requestParser.ts
@@ -5,6 +5,12 @@ import { LicensePlateQueryType } from '../controllers/LicensePlateQuery.controll
 import { z } from 'zod';
 import { CityType } from '../controllers/City.controller';
 
+/**
+ * This function is supposed to be able to handle existing data checks for every database table by
+ * using dependency injection to pass a controller together with its types and functions into this function.
+ * It has partly been commented out, as it is currently non-functioning but will soon be revisited
+ * which is why it has not been removed.
+ * */
 export async function checkEntryExists(
   controller: ControllerTypes,
   body: LicensePlateRequestType | LicensePlateQueryType | CityType
