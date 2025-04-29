38d3ff7832def39f0c8ff0378612c0fb52510452
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2025-04-29 19:27:12 +0200

    add wildcard info using new alert component

--- apps/web/components/request-form.tsx
+++ apps/web/components/request-form.tsx
@@ -185,6 +185,28 @@ export default function LicensePlateForm() {
             </div>
           </div>
 
+          <Alert className="bg-blue-50 text-blue-800 border-blue-200 my-2">
+            <AlertDescription>
+              <p className="font-medium">Wildcards:</p>
+              <ul className="list-disc list-inside text-sm mt-1">
+                <li>
+                  Use * to match any sequence of characters (including none)
+                </li>
+                <li>Use ? to match exactly one character</li>
+                <li>Maximum of 3 "?" wildcards total across both fields</li>
+                <li>Both fields cannot have "*" simultaneously</li>
+                <li>
+                  In the numbers field, "*" can only be used alone (not with
+                  other numbers)
+                </li>
+                <li>
+                  All combinations will respect the maximum field lengths (2
+                  letters, 4 numbers)
+                </li>
+              </ul>
+            </AlertDescription>
+          </Alert>
+
           <div className="pt-4">
             <Button
               type="submit"
