121ab4937ab42bea9e11caa16d2a10bf5daeb44e
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2025-04-30 22:49:26 +0200

    add more pages

---
+++ apps/web/app/profile/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import NavBar from 'apps/web/components/nav-bar'
+
+export default function ProfilePage() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <NavBar />
+      This is the profile page
+    </main>
+  )
+}
