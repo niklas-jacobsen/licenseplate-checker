0541a1086607b124aa4accc42ed7cc51ec57b57d
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2025-12-14 17:07:28 +0100

    cleanup unused code

--- apps/api/prisma/data-source.ts
+++ apps/api/prisma/data-source.ts
@@ -2,7 +2,12 @@ import { PrismaClient } from '@prisma/client'
 import { PrismockClient } from 'prismock'
 import { ENV } from '../src/env'
 
+declare global {
+  var __db: PrismaClient | undefined
+}
+
 let prisma: PrismaClient
+
 if (ENV.NODE_ENV === 'test') {
   console.log('Using mock client')
   prisma = new PrismockClient()
@@ -10,6 +15,17 @@ if (ENV.NODE_ENV === 'test') {
   prisma = new PrismaClient({
     datasourceUrl: ENV.DATABASE_URL,
   })
+}
+
+if (ENV.NODE_ENV === 'production') {
+  prisma = new PrismaClient({ log: ['error'] })
+  prisma.$connect()
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient({ log: ['error', 'warn'] })
+    global.__db.$connect()
+  }
+  prisma = global.__db
 }
 
 export { prisma }
