ddb2ce9910d5e5cd385178a7adc4efe7c85e7a9a
Niklas Jacobsen <niklas.jacobsen@code.berlin>
2026-02-19 02:46:30 +0100

    update custom fonts location into public folder

---
+++ apps/web/lib/utils.ts
@@ -0,0 +1,16 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function toTitleCase(str: string) {
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    .join(' ')
+}
